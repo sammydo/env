dde75463a1ecebd94e517e69b85743544c9bdfda
Samuel Udoh <sudoh@spartaglobal.co>
2017-04-21 18:39:31 +0100

    added

---
+++ cookbook/node-server/test/integration/default/serverspec/default_spec.rb
@@ -0,0 +1,41 @@
+require 'serverspec'
+
+#required by severspec
+set :backend, :exec
+
+
+describe package('nginx') do
+  it { should be_installed }
+end
+
+describe service('nginx') do
+  it { should be_running }
+  it { should be_enabled }
+end
+
+describe command('node -v') do
+  its(:stdout) { should match /6\.10\.0/ }
+end
+
+describe port(80) do
+  it { should be_listening }
+end
+
+describe package('git') do
+  it { should be_installed }
+end
+
+# describe command('git --version') do
+#   its(:stdout) { should match /2\.7\.4/ }
+# end
+
+describe package('pm2') do
+  it { should be_installed.by('npm') }
+end
+
+describe package('gulp') do
+  it { should be_installed.by('npm') }
+end
+describe package('bower') do
+  it { should be_installed.by('npm') }
+end
