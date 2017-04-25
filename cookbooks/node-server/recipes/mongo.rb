421b172ff091768e14b802f647152e12a439700a
Samuel Udoh <sudoh@spartaglobal.co>
2017-04-25 10:55:15 +0100

    changed magic shell

--- cookbooks/node-server/recipes/mongo.rb
+++ cookbooks/node-server/recipes/mongo.rb
@@ -6,7 +6,3 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 include_recipe 'apt'
 include_recipe 'mongodb3::default'
-
- magic_shell_environment 'MONGODB_URI' do
-   value 'mongodb://192.168.10.101'
- end
