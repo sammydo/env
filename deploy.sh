5675b360da819f56d8c7cfb67c589ebf5e9f7ca8
Samuel Udoh <sudoh@spartaglobal.co>
2017-04-25 11:53:39 +0100

    added production.json file

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,7 @@ sudo chef-client --local-mode --runlist 'recipe[node-server]'
 cd ~/digital_profiles
 git pull
 sudo npm install
-export MONGODB_URI=mongodb://34.248.151.171
+export MONGODB_URI=mongodb://3git fetch --all
+4.248.151.171
 pm2 kill
 pm2 start index.js
